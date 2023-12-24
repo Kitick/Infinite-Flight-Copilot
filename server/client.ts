a327bc08217c702ffe29aa7a4356d0fa63d1e0c6
Kevin Munson <kitick538@gmail.com>
2023-12-24 02:14:24 -0800

    client variables and methods are private and added timeout for udp packets

--- server/client.ts
+++ server/client.ts
@@ -2,117 +2,93 @@ const Net = require("net");
 const UDP = require("dgram");
 
 class Client {
-    device = new Net.Socket();
-    scanner = UDP.createSocket("udp4");
-    scanning = false;
-    active = false;
-    dataBuffer:number[] = [];
-    manifest = new Map<string, Item>();
+    #socket:any;
+    #address:string = "";
+    #device = new Net.Socket();
+    #scanner = UDP.createSocket("udp4");
+    #scannerTimeout:NodeJS.Timeout|null = null;
+    #active = false;
+    #dataBuffer:number[] = [];
+    #manifest = new Map<string, Item>();
 
-	constructor(public socket:any, public address = ""){
+	constructor(socket:any, address = ""){
+        this.#socket = socket;
+        this.#address = address;
+
 		this.#initManifest();
 
-		this.device.on("data", (buffer:Buffer) => {
-            console.log(this.address + " Rx\t\t\t", buffer);
+		this.#device.on("data", (buffer:Buffer) => {
+            console.log(this.#address + " Rx\t\t\t", buffer);
 
-			for(let binary of buffer){this.dataBuffer.push(binary);}
+			for(let binary of buffer){this.#dataBuffer.push(binary);}
 
 			this.#validate();
 		});
 
-		this.device.on("error", (error:any) => {
+		this.#device.on("error", (error:any) => {
 			if(error.code === "ECONNREFUSED"){
-				this.log(this.address + " TCP Connection Refused");
+				this.log(this.#address + " TCP Connection Refused");
 			}
 		});
 
-		this.log(this.address + " TCP Socket Created");
+        this.#scanner.on("message", (data:any, info:any) => {
+			this.#address = info.address;
+			this.log(this.#address + " UDP Packet Found");
+
+			this.#closeScanner();
+            this.connect();
+		});
+
+		this.log(this.#address + " TCP Socket Created");
 	}
 
+    get #scanning(){return this.#scannerTimeout !== null;}
+
 	#initManifest():void {
-		this.manifest = new Map();
+		this.#manifest = new Map();
 		this.addItem(new Item(-1, 4, "manifest"));
 	}
 
-	log(message:string):void {
-		this.socket.emit("log", message);
-		console.log(message);
-	}
+    #closeScanner(){
+        if(!this.#scanning){return;}
 
-	#findAddress(callback = () => {}):void {
-		if(this.scanning){
+        clearTimeout(this.#scannerTimeout as NodeJS.Timeout);
+        this.#scannerTimeout = null;
+
+        this.#scanner.close();
+    }
+
+	#findAddress():void {
+		if(this.#scanning){
 			this.log("Already Searching for UDP Packets");
 			return;
 		}
 
 		this.log("Searching for UDP Packets...");
 
-		this.scanner.on("message", (data:any, info:any) => {
-			this.address = info.address;
-			this.log(this.address + " UDP Packet Found");
+        this.#scannerTimeout = setTimeout(() => {
+            this.#closeScanner();
+            this.log("UDP Search Timed out");
+        }, 10000);
 
-			this.scanner.close();
-			this.scanning = false;
-
-			callback();
-		});
-
-		this.scanning = true;
-		this.scanner.bind(15000);
-	}
-
-	connect():void {
-		this.log(this.address + " Attempting TCP Connection");
-
-		if(this.address === ""){
-			this.#findAddress(() => {this.connect();});
-			return;
-		}
-
-		if(this.active){
-			this.log(this.address + " TCP Already Active");
-			this.socket.emit("ready", this.address);
-			return;
-		}
-
-		this.device.connect({host:this.address, port:10112}, () => {
-			this.log(this.address + " TCP Established, Requesting Manifest");
-
-			this.active = true;
-			this.readState("manifest");
-		});
-	}
-
-	close():void {
-		if(this.scanning){
-			this.scanner.close();
-			this.scanning = false;
-		}
-
-		if(this.active){
-            this.active = false;
-
-			this.device.end(() => {
-				this.log(this.address + " TCP Closed");
-			});
-		}
+		this.#scanner.bind(15000);
 	}
 
 	#validate():void {
-		if(this.dataBuffer.length < 9){return;}
+		if(this.#dataBuffer.length < 9){return;}
 
-		const length = Buffer.from(this.dataBuffer.slice(4, 8)).readInt32LE() + 8;
+		const length = Buffer.from(this.#dataBuffer.slice(4, 8)).readInt32LE() + 8;
 
-		if(this.dataBuffer.length < length){return;}
+		if(this.#dataBuffer.length < length){return;}
 
-		const id = Buffer.from(this.dataBuffer.slice(0, 4)).readInt32LE();
-		const data = Buffer.from(this.dataBuffer.slice(8, length));
+		const id = Buffer.from(this.#dataBuffer.slice(0, 4)).readInt32LE();
+		const data = Buffer.from(this.#dataBuffer.slice(8, length));
 
-		this.dataBuffer.splice(0, length);
+		this.#dataBuffer.splice(0, length);
 
 		this.#processData(id, data);
 
-		if(this.dataBuffer.length > 0){this.#validate();}
+		if(this.#dataBuffer.length > 0){this.#validate();}
 	}
 
 	#processData(id:number, data:Buffer):void {
@@ -133,8 +109,8 @@ class Client {
 				this.addItem(item);
 			});
 
-			this.log(this.address + " Manifest Built, API Ready");
-			this.socket.emit("ready", this.address);
+			this.log(this.#address + " Manifest Built, API Ready");
+			this.#socket.emit("ready", this.#address);
 
             return;
 		}
@@ -155,6 +131,45 @@ class Client {
 		return buffer;
 	}
 
+    log(message:string):void {
+		this.#socket.emit("log", message);
+		console.log(message);
+	}
+
+    connect():void {
+		this.log(this.#address + " Attempting TCP Connection");
+
+		if(this.#address === ""){
+			this.#findAddress();
+			return;
+		}
+
+		if(this.#active){
+			this.log(this.#address + " TCP Already Active");
+			this.#socket.emit("ready", this.#address);
+			return;
+		}
+
+		this.#device.connect({host:this.#address, port:10112}, () => {
+			this.log(this.#address + " TCP Established, Requesting Manifest");
+
+			this.#active = true;
+			this.readState("manifest");
+		});
+	}
+
+	close():void {
+		if(this.#scanning){this.#closeScanner();}
+
+		if(this.#active){
+            this.#active = false;
+
+			this.#device.end(() => {
+				this.log(this.#address + " TCP Closed");
+			});
+		}
+	}
+
 	readState(itemID:string, callback = () => {}):void {
 		const item = this.getItem(itemID);
 
@@ -168,8 +183,8 @@ class Client {
 
 		const buffer = this.#initalBuffer(item.id, 0);
 
-		this.device.write(buffer);
-		console.log(this.address + " Tx " + itemID + " (" + item.id + ")\t", buffer);
+		this.#device.write(buffer);
+		console.log(this.#address + " Tx " + itemID + " (" + item.id + ")\t", buffer);
 	}
 
 	writeState(itemID:string):void {
@@ -180,22 +195,22 @@ class Client {
 
 		buffer = Buffer.concat([buffer, item.buffer]);
 
-		this.device.write(buffer);
-		console.log(this.address + " Tx " + itemID + " (" + item.id + ")\t", buffer);
+		this.#device.write(buffer);
+		console.log(this.#address + " Tx " + itemID + " (" + item.id + ")\t", buffer);
 	}
 
 	addItem(item:Item):void {
-        this.manifest.set(item.id.toString(), item);
-		this.manifest.set(item.name, item);
+        this.#manifest.set(item.id.toString(), item);
+		this.#manifest.set(item.name, item);
 
 		if(item.alias !== null){
-            this.manifest.set(item.alias, item);
+            this.#manifest.set(item.alias, item);
         }
 	}
 
     getItem(itemID:string):Item|undefined {
-        const item = this.manifest.get(itemID);
-        if(item === undefined){this.log(this.address + " Invalid Item " + itemID);}
+        const item = this.#manifest.get(itemID);
+        if(item === undefined){this.log(this.#address + " Invalid Item " + itemID);}
         return item;
     }
 }
